0862d76f89efef9aae41513489566c374f990cba
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-11-11 08:52:09 +0100

    titulo repetido

--- Tema2Python/ejercicioDiccionarios.py
+++ Tema2Python/ejercicioDiccionarios.py
@@ -17,24 +17,6 @@
 # • Borrar una palabra (ustedes decidís la mejor forma).
 # • Ordenar el diccionario por orden alfabético.
 
-# Estoy dando clases de inglés para el B2 y me gustaría que me ayudarais a crear un diccionario para ver cómo
-# se dicen palabras con significado "raro" españolas en inglés. Se hará todo en el mismo archivo, no se crean
-# clases. Por ejemplo, "Cabeza: (en el sur de España) amigo/colega", aro: sí, de acuerdo. Consideraremos que
-# no se repiten palabras con el mismo nombre, es decir, que la palabra "take" no aparece dos veces en el
-# diccionario y que, si tiene varios significados, estos pueden ir en el mismo String "significado", por ejemplo,
-# for---->por, para, durante.
-# Se debe utilizar un diccionario que se puede instanciar con algunos elementos de inicio y un menú para cada
-# uno de las siguientes funcionalidades:
-# • Agregar una nueva palabra por teclado.
-# • Imprimir el diccionario completo "en bonito".
-# • Buscar una palabra por nombre y mostrar su significado evitando que salta un error si la palabra no se
-# encuentra.
-# • Modificar una palabra, modificando únicamente el significado de esta por otro nuevo, leído por
-# teclado.
-# • Pedir al usuario sus dos palabras favoritas con los significados y combinar este con el ya creado
-# modificando el original.
-# • Borrar una palabra (ustedes decidís la mejor forma).
-# • Ordenar el diccionario por orden alfabético.
 
 Esp_Ing = {
     'Cabeza' : 'Amigo, parte superior del cuerpo humano, parte principal o inicial de algo ,líder' , 
